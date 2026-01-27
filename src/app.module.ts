08300884101c2a6d7fd8c1befd20213e58cf87fb
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-01-26 21:00:00 -0300

    chore: initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(databaseConfig()),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig = (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+  username: process.env.DATABASE_USER || 'matchmaking',
+  password: process.env.DATABASE_PASSWORD || 'matchmaking123',
+  database: process.env.DATABASE_NAME || 'matchmaking',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // Em produção, usar migrations
+});
